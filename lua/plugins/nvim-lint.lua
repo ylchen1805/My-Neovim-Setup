027424b2546e28800e6f24965826e0ea49e06f80
allen <ylchen1805@gmail.com>
2026-05-11 15:06:47 +0800

    Modify the nvim-lint to disable the MD013 lint length error

---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "mfussenegger/nvim-lint",
+        opts = {
+            linters = {
+                ["markdownlint-cli2"] = {
+                    args = {
+                        "--config",
+                        vim.fn.stdpath("config") .. "/.markdownlint-cli2.jsonc",
+                        "--",
+                    },
+                },
+            },
+        },
+    },
+}
