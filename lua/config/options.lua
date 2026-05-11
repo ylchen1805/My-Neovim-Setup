5c297daac6da4158509cb0ef58268b0d5c84efd8
allen <ylchen1805@gmail.com>
2026-05-11 14:41:48 +0800

    Configured pyright and ruff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -20,3 +20,9 @@ opt.shiftwidth = 4
 
 -- Ensure auto-indentation rounds to the nearest shiftwidth
 opt.shiftround = true
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
