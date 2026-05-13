2067d1178bbec72e7b71b6f936a42675fd5246c7
allen <ylchen1805@gmail.com>
2026-05-13 18:28:15 +0800

    Add plugins and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 單次 Esc 給 terminal 程式
+-- 連按兩次才退出 terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
