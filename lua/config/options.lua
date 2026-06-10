c5d8f598bdcab42c6b197642cd3ef73914bc8f3a
Allen <ylchen1805@gmail.com>
2026-06-10 16:02:11 +0800

    Add OSC 53 clipboard setting for yane and paste cross machines

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -26,3 +26,19 @@ opt.shiftround = true
 vim.g.lazyvim_python_lsp = "pyright"
 -- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
+
+-- unnamedplus 使用系統剪貼簿
+-- vim.opt.clipboard:append("unnamedplus")
+vim.opt.clipboard = { "unnamed", "unnamedplus" }
+-- 使用 OSC 52
+vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+}
